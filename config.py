9f73054a054e876847f0958a4f6c8aa1d50cf193
icaresakr <48725540+icaresakr@users.noreply.github.com>
2021-11-30 16:08:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,13 @@
 
 LOAD_DIR = r"/home/vita-w11/Desktop/Ics/3Dpifpaf_r/" #directory of the bag files
 
-SAVE_DIR = r"/home/vita-w11/Desktop/Ics/3Dpifpaf_r/df/" #directory of the saved dataframes
+SAVE_DIR = r"/home/vita-w11/Desktop/Ics/3Dpifpaf_r/df/" #directory of the saved files
 
 BAG_NAME = r"cam2_record_27_05_2021__13_08_49.bag" #bag file name, don't care about it if running the batch
+
+RUN_BATCH = True # True if convert all bag files inside LOAD_DIR
+
+SAVE_ANNOTATED_VIDEO = False # export the rgb and depth video with overlayed pifpaf keypoints.
 
 PRE_PROCESS = {
     'selected': ['temporal_filter', 'hole_filling_filter'],
@@ -25,8 +29,6 @@ POST_PROCESS = {
 USE_CUDA = True # well use if available
 
 VISUALIZATION = False # show plots and video
-
-RUN_BATCH = True # True if convert all bag files inside LOAD_DIR
 
 CAMERA = { # camera recording parameter, attention when running batch if different bags have different resolutions, it cannot be done
     'resolution': [640, 480],
